0051f28434917c410cbc294ec52938c371bf30e2
Abarna Pandian <abarnapandian2000@gmail.com>
2021-01-10 22:57:24 +0530

    Add files via upload

---
+++ Area of Circle.py
@@ -0,0 +1,6 @@
+PI = 3.14
+radius = float(input("Please Enter the radius of a circle: "))
+area = PI * radius * radius
+circumference = 2 * PI * radius
+print(" Area Of a Circle = %.2f" %area)
+print(" Circumference Of a Circle = %.2f" %circumference)
