93702a3b4a8fa814879e4385dbb15e3a636f626f
Abarna Pandian <abarnapandian2000@gmail.com>
2021-01-28 19:08:36 +0530

    Add files via upload

---
+++ Matrix/Matrix Subtraction1.py
@@ -0,0 +1,17 @@
+#Another Method for Matrix Subtraction
+
+x = [[12,7,3],
+    [4,5,6],
+    [7,8,9]]
+y = [[5,8,1],
+    [6,7,3],
+    [4,5,9]]
+result = [[0,0,0],
+        [0,0,0],
+        [0,0,0]]
+for i in range(len(x)):
+    for j in range(len(x[0])):
+        result[i][j] = x[i][j] - y[i][j]
+print("The Subtracted Matrix is : ")
+for r in result:
+    print(r)
