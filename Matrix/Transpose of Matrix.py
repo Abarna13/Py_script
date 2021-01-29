69f7919bccb68160b70bc0cdcae26155d8c18c09
Abarna Pandian <abarnapandian2000@gmail.com>
2021-01-29 23:03:11 +0530

    Add files via upload

---
+++ Matrix/Transpose of Matrix.py
@@ -0,0 +1,14 @@
+x = [[12,7],
+     [4,5],
+     [3,8]]
+result = [[0,0,0],
+         [0,0,0]]
+for i in range(len(x)):
+    for j in range(len(x[0])):
+        result[j][i] = x[i][j]
+print("The given Matrix is : ")
+for r in x:
+    print(r)
+print("The Transpose Matrix is : ")
+for r in result:
+    print(r)
