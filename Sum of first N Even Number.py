3ddcb652effb9ab634c55da8e73b1f4471326f2d
Abarna Pandian <abarnapandian2000@gmail.com>
2021-07-23 15:55:57 +0530

    Delete Sum of first N Even Number.py

--- Sum of first N Even Number.py
+++
@@ -1,6 +0,0 @@
-num = int(input("Print sum of even numbers till : "))
-total = 0
-for i in range(1, num + 1):
-    if((i % 2) == 0):
-        total = total + i
-print("Sum of even numbers from 1 to", num, "is :", total)
