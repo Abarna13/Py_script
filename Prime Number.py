d7389db767c84e9f77e560e00147f01d177eb094
Abarna Pandian <abarnapandian2000@gmail.com>
2021-07-23 16:01:43 +0530

    Delete Prime Number.py

--- Prime Number.py
+++
@@ -1,9 +0,0 @@
-a = int(input("Enter a Number : "))
-if (a == 2):
-    print("The Given Number",a,"is a Prime Number")
-for i in range(1,a):
-    if(a%i==0):
-        print("The Given Number",a,"is not a Prime Number")
-        break
-    else:
-        print("It is a Prime Number")
