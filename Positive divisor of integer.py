19dcf212e0f7ab1955142d841ac65abce3451b3b
Abarna Pandian <abarnapandian2000@gmail.com>
2021-07-23 16:01:28 +0530

    Delete Positive divisor of integer.py

--- Positive divisor of integer.py
+++
@@ -1,9 +0,0 @@
-#Python program to read positive integer and print all positive divisor of that intger.
-
-n = int(input("Enter the Number\t"))
-if(n<=0):
-    print("It needs only the positive integer.")
-else:
-    for i in range(1,n+1):
-        if(n%i==0):
-            print(i,end=" ")
