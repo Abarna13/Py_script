eca8757b43f036a274552e949efefd4edce9b037
Abarna Pandian <abarnapandian2000@gmail.com>
2021-07-23 15:59:11 +0530

    Delete Search element in list.py

--- Search element in list.py
+++
@@ -1,7 +0,0 @@
-#Searching an element in a list using linear search Technique
-
-list = [1,78,3,56,6,7,98]
-search = int(input("enter the search element : "))
-for i in range(0,len(list)):
-    if search == list[i]:
-        print(str(search)+(" Found at Position ")+str(i))
