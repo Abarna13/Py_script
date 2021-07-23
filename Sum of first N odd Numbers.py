48552f874c2ff8225314da3cf3acbbad7cb6046a
Abarna Pandian <abarnapandian2000@gmail.com>
2021-07-23 15:55:05 +0530

    Delete Sum of first N odd Numbers.py

--- Sum of first N odd Numbers.py
+++
@@ -1,6 +0,0 @@
-num = int(input("Print sum of odd numbers till : "))
-sum = 0
-for i in range(1, num + 1):
-    if(not (i % 2) == 0):
-        sum += i
-print("Sum of odd numbers from 1 to", num, "is :", sum)
