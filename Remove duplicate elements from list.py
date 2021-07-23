d5b9c55cfc937def3363efd449eafe3b17d385ae
Abarna Pandian <abarnapandian2000@gmail.com>
2021-07-23 15:59:36 +0530

    Delete Remove duplicate elements from list.py

--- Remove duplicate elements from list.py
+++
@@ -1,9 +0,0 @@
-a = [10,20,30,20,10,50,60,40,80,50,40]
-print("The Given list is : ",a)
-dup_items = set()
-uniq_items = []
-for x in a:
-    if x not in dup_items:
-        uniq_items.append(x)
-        dup_items.add(x)
-print("The list with duplicates removed is : ",dup_items)
