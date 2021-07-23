737ae40baf6e7ab5c210805c9f969d44397727c7
Abarna Pandian <abarnapandian2000@gmail.com>
2021-07-23 15:59:23 +0530

    Delete Reverse.py

--- Reverse.py
+++
@@ -1,5 +0,0 @@
-#Python program for accept the value from the user and reverse it
-
-word = input("Enter a word to reverse: ")
-for char in range(len(word)-1,-1,-1):
-    print(word[char],end = " ")
