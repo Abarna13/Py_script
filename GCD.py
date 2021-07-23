38aed086fdcfd9a92a81f5495d3c407a791fd93c
Abarna Pandian <abarnapandian2000@gmail.com>
2021-07-23 16:06:37 +0530

    Delete GCD.py

--- GCD.py
+++
@@ -1,6 +0,0 @@
-num1 = int(input("Enter the first Number : "))
-num2 = int(input("Enter the second Number : "))
-for i in range(1,min(num1,num2)+1):
-    if (num1%i==0 and num2%i==0):
-        gcd=i
-print("Gcd of",num1,"and",num2,"is : ",gcd)
