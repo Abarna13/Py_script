ce3c8a5b40cfd6661b5378ec82cfdd71c5f8f032
Abarna Pandian <abarnapandian2000@gmail.com>
2021-07-23 15:57:57 +0530

    Delete Square root using newton method.py

--- Square root using newton method.py
+++
@@ -1,7 +0,0 @@
-n = int(input("Enter a Number : "))
-m = int(input("Enter the number of iterations: "))
-approx = (1/2) * n
-for i in range(0,m):
-    better = (1/2) * (approx+n/approx)
-    approx = better
-    print(approx)
