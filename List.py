4206be222de1d447f01d5cee31d45cc0ae1b1cba
Abarna Pandian <abarnapandian2000@gmail.com>
2021-07-23 16:06:26 +0530

    Delete List.py

--- List.py
+++
@@ -1,8 +0,0 @@
-string = input("Enter the String: ")
-print("The Length of a string",len(string))
-print("The String Reverse is",string[::-1])
-print("The string display 3 times",string*3)
-string2 = input("Enter the Second String : ")
-print("The Concatenated strings are : ",string+string2)
-str1 = "South India"
-print(str1[6:])
