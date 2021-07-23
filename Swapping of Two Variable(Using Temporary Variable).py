f9aab7e317e4ab438581b500f42022adde3b5015
Abarna Pandian <abarnapandian2000@gmail.com>
2021-07-23 15:54:25 +0530

    Delete Swapping of Two Variable(Using Temporary Variable).py

--- Swapping of Two Variable(Using Temporary Variable).py
+++
@@ -1,7 +0,0 @@
-x = int(input("Enter the value of X : "))
-y = int(input("Enter the value of Y : "))
-temp = x
-x = y
-y = temp
-print("The Value of X after Swapping : ",x)
-print("The Value of Y after Swapping : ",y)
