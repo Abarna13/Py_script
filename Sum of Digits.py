3d2960a368c69534eca65bc24b1072c2b7c47b9b
Abarna Pandian <abarnapandian2000@gmail.com>
2021-07-23 15:56:11 +0530

    Delete Sum of Digits.py

--- Sum of Digits.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Enter a Number : "))
-result = 0
-sum = 0
-while(n>0):
-    result = n % 10
-    sum = sum + result
-    n = int(n/10)
-print("The Sum of Digits of a Given Number is",sum)
