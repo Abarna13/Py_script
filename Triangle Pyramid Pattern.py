62f94369d20a6767508ab25a924886c29599a5d5
Abarna Pandian <abarnapandian2000@gmail.com>
2021-07-23 15:52:48 +0530

    Delete Triangle Pyramid Pattern.py

--- Triangle Pyramid Pattern.py
+++
@@ -1,9 +0,0 @@
-rows = int(input("Enter the row value : "))
-space = rows - 1
-for i in range(0, rows):
-    for j in range(0, space):
-        print(end = " ")
-    space = space - 1
-    for k in range(0, i+1):
-        print("*", end = " ")
-    print()
